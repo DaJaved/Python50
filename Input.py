577939e9c57d70d0e2345b701ff9c00a4f877de7
DaJaved <154273576+DaJaved@users.noreply.github.com>
2023-12-18 20:06:29 +0000

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,15 @@
+# In built funtion to prompt code
+fullname = input("What's your name? ")
+
+
+#will have double space due to print default parameters
+print("Hello ",fullname) 
+
+#will have two line due to print default parameters
+print("Hello ")
+print(fullname) 
+
+#will concatinate str and variable
+print("Hello " + fullname) 
+
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=false)
